f28bbf0553f7e6b9c8e2f95dcb998e304f559eb8
nemorober <35772060+nemorober@users.noreply.github.com>
2018-01-24 16:03:42 +0100

    zazahlaunch

---
+++ zazah/resources/templates/footer.php
@@ -0,0 +1,30 @@
+<style>
+.footer {
+
+    background-color:white;
+    color: #777;
+    text-align: center;
+    border-style: solid;
+    border-width: 1px 0px 0px 0px;
+    border-color: #8266a8;
+}
+</style>
+<div class="push">
+</div>
+</div>
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-lg-2">
+    </div>
+    <div class="col-lg-8">
+      <div class="footer" style="overflow-x: hidden; overflow-y: hidden;">
+        <p style="font-size: 75%">© Zazah Greta Van den Broeck 2017 - Website by Nemo Rober</p>
+      </div>
+    </div>
+    <div class="col-lg-2">
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
